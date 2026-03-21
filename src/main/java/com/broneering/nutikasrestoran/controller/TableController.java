4b5e3f7d01295f3f2f127f96d77ba0dac8d58d09
Lisbeth Lepp <LisbethLepp12@gmail.com>
2026-03-21 17:06:17 +0200

    Lisatud laudade filtreering

--- src/main/java/com/broneering/nutikasrestoran/controller/TableController.java
+++ src/main/java/com/broneering/nutikasrestoran/controller/TableController.java
@@ -3,11 +3,14 @@ package com.broneering.nutikasrestoran.controller;
 import com.broneering.nutikasrestoran.object.RestaurantTable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/tables")
@@ -29,14 +32,31 @@ public class TableController {
 
             //Lisan talle juhuslikult omadusi ning lõpuks listi
             if (random.nextBoolean()) {
-                table.getFeatures().add("aknaga");
+                table.getFeatures().add("Akna all");
             }
             tables.add(table);
         }
     }
 
     @GetMapping
-    public List<RestaurantTable> getAllTables() {
-        return tables;
+    // @RequestParam - extact query parameters, form parameters, files from a web request
+    // and bind them to method parameters in a controller
+    // You can specify if the param is required and set default values if needed
+    public List<RestaurantTable> getTables(@RequestParam(required = false) Integer size) {
+    if (size == null) {
+        return tables; //Näitan kõike
+    }
+        //.stream() takes data structure and gives a sequence of elements you can
+        // transform, filter, sort, combine
+        return tables.stream()
+                //Kas lauas on piisavalt kohti
+                .filter(table -> table.getSize() >= size)
+                //Kas laud on vaba
+                .filter(table -> !table.occupied)
+                //Lisame sorteerimise sobilikumast lauast, vähem sobilikumani
+                .sorted(Comparator.comparingInt(RestaurantTable::getSize))
+                //Leiame sobivad lauad
+                .collect(Collectors.toList());
+
     }
 }
