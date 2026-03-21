a8cb63654d1046c3c9414341047ced73c1a8722c
Lisbeth Lepp <LisbethLepp12@gmail.com>
2026-03-21 11:40:40 +0200

    Baaskood

---
+++ src/main/java/com/broneering/nutikasrestoran/NutikasRestoranApplication.java
@@ -0,0 +1,13 @@
+package com.broneering.nutikasrestoran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutikasRestoranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutikasRestoranApplication.class, args);
+	}
+
+}
